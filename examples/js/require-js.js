d7f1956f1b24f7fe43ae1b3c6d18a92e5f8b7edd
David DeSandro <desandrocodes@gmail.com>
2014-01-23 07:39:33 -0500

    Require JS example

--- examples/js/require-js.js
+++ examples/js/require-js.js
@@ -1,21 +1,32 @@
 /*global requirejs: false*/
 
+/*
+// bower components
 requirejs.config({
   baseUrl: '../bower_components'
-  // OR
-  // paths: {
-  //   eventie: '../bower_components/eventie',
-  //   'doc-ready': '../bower_components/doc-ready',
-  //   eventEmitter: '../bower_components/eventEmitter',
-  //   'get-style-property': '../bower_components/get-style-property',
-  //   'get-size': '../bower_components/get-size',
-  //   'matches-selector': '../bower_components/matches-selector',
-  //   outlayer: '../bower_components/outlayer'
-  // }
 });
 
 requirejs( [ '../js/packery' ], function( Packery ) {
 
   new Packery( document.querySelector('#basic') );
 
+});
+// */
+
+/*
+// packery.pkgd.js
+requirejs( [
+  'js/packery.pkgd.js',
+  'require'
+], function( pkgd, require ) {
+  require( ['packery/js/packery'], function ( Packery ) {
+    new Packery('#basic');
+  });
+});
+// */
+
+requirejs( [
+  'js/packery.pkgd.js'
+], function( Packery ) {
+  new Packery('#basic');
 });
