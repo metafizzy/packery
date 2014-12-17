ae57f5ccb0c81f3c714015c998bd57316c5912ac
David DeSandro <desandrocodes@gmail.com>
2014-12-17 12:05:13 -0500

    add sub-pixel fit test; Ref #227, #42

---
+++ test/sub-pixel-fit.js
@@ -0,0 +1,39 @@
+( function() {
+
+'use strict';
+
+test( 'sub-pixel fit', function() {
+
+  var pckry = new Packery( '#sub-pixel-fit', {
+    itemSelector: '.item',
+    transitionDuration: 0
+  });
+
+  function getItemsTotalY() {
+    var y = 0;
+    for ( var i=0, len = pckry.items.length; i < len; i++ ) {
+      var item = pckry.items[i];
+      y += item.rect.y;
+    }
+    return y;
+  }
+
+  // iterate over multiple container widths
+  for ( var containerWidth = 290; containerWidth < 310; containerWidth++ ) {
+    pckry.element.style.width = containerWidth + 'px';
+    pckry.layout();
+    equal( 0, getItemsTotalY(), 'items fit at ' + containerWidth + 'px' );
+  }
+
+  // set grid sizer and do it again
+  pckry.options.columnWidth = '.grid-sizer';
+
+  for ( containerWidth = 290; containerWidth < 310; containerWidth++ ) {
+    pckry.element.style.width = containerWidth + 'px';
+    pckry.layout();
+    equal( 0, getItemsTotalY(), 'items fit with columnWidth at ' + containerWidth + 'px' );
+  }
+
+});
+
+})();
